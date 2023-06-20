a6bf5791bfd2af5251caf5c51a04a14b8b523dd0
Emmy Killett <57272627+killett@users.noreply.github.com>
2023-06-20 11:09:10 -0700

    refactor: build SpectralDistribution from a wavelength-value dict

--- colour.py
+++ colour.py
@@ -4,7 +4,12 @@ import colour
 # Define your spectral power distribution data.
 wavelengths = np.arange(360, 781)  # For example, from 360 to 780 nm
 values = np.random.random_sample(len(wavelengths))  # Your actual SPD data here
-spd = colour.SpectralDistribution(values, wavelengths)
+
+# Create a dictionary that maps wavelengths to values
+spd_data = dict(zip(wavelengths, values))
+
+# Create a SpectralDistribution object
+spd = colour.SpectralDistribution(spd_data)
 
 # Define the standard observer color matching functions.
 cmfs = colour.STANDARD_OBSERVERS_CMFS['CIE 1931 2 Degree Standard Observer']
