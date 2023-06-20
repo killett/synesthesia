a9782488826ecd15bb61c4a1e2342f91826fcf0b
Emmy Killett <57272627+killett@users.noreply.github.com>
2023-06-20 11:07:34 -0700

    feat: add colour.py prototype for CIE XYZ spectral conversion

---
+++ colour.py
@@ -0,0 +1,15 @@
+import numpy as np
+import colour
+
+# Define your spectral power distribution data.
+wavelengths = np.arange(360, 781)  # For example, from 360 to 780 nm
+values = np.random.random_sample(len(wavelengths))  # Your actual SPD data here
+spd = colour.SpectralDistribution(values, wavelengths)
+
+# Define the standard observer color matching functions.
+cmfs = colour.STANDARD_OBSERVERS_CMFS['CIE 1931 2 Degree Standard Observer']
+
+# Compute the XYZ tristimulus values.
+XYZ = colour.spectral_to_XYZ(spd, cmfs)
+
+print(XYZ)
