8a105a778e5ad728b5e627f857096b546e425ff6
Emmy Killett <57272627+killett@users.noreply.github.com>
2023-06-20 20:21:40 -0700

    fix: drop unnecessary CMFS alias, import name directly

--- colour.py
+++ colour.py
@@ -1,6 +1,6 @@
 import numpy as np
 from colour.colorimetry import SpectralDistribution, sd_to_XYZ
-from colour.colorimetry import STANDARD_OBSERVERS_CMFS as CMFS
+from colour.colorimetry import STANDARD_OBSERVERS_CMFS
 
 # Define your spectral power distribution data.
 wavelengths = np.arange(360, 781)  # For example, from 360 to 780 nm
@@ -10,7 +10,7 @@ values = np.random.random_sample(len(wavelengths))  # Your actual SPD data here
 spd = SpectralDistribution(values, wavelengths)
 
 # Define the standard observer color matching functions.
-cmfs = CMFS['CIE 1931 2 Degree Standard Observer']
+cmfs = STANDARD_OBSERVERS_CMFS['CIE 1931 2 Degree Standard Observer']
 
 # Compute the XYZ tristimulus values.
 XYZ = sd_to_XYZ(spd, cmfs)
