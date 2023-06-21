d45f5764b4400de9c8905d87d5e6c87e8dc52be6
Emmy Killett <57272627+killett@users.noreply.github.com>
2023-06-20 21:06:00 -0700

    fix: settle on MSDS_CMFS_STANDARD_OBSERVER for CMFS lookup
    Final import layout after several colour-science API names were
    tried in recent commits.

--- colour.py
+++ colour.py
@@ -1,6 +1,6 @@
 import numpy as np
-from colour import SpectralDistribution
-from colour.colorimetry import get_cmfs, sd_to_XYZ
+from colour.colorimetry import MSDS_CMFS_STANDARD_OBSERVER
+from colour import SpectralDistribution, sd_to_XYZ
 
 # Define your spectral power distribution data.
 wavelengths = np.arange(360, 781)  # For example, from 360 to 780 nm
@@ -10,7 +10,7 @@ values = np.random.random_sample(len(wavelengths))  # Your actual SPD data here
 spd = SpectralDistribution(values, wavelengths)
 
 # Define the standard observer color matching functions.
-cmfs = get_cmfs('CIE 1931 2 Degree Standard Observer')
+cmfs = MSDS_CMFS_STANDARD_OBSERVER['CIE 1931 2 Degree Standard Observer']
 
 # Compute the XYZ tristimulus values.
 XYZ = sd_to_XYZ(spd, cmfs)
